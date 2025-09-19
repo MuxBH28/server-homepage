5f61526c3e788af451e748341638468c614d0157
MuxBH28 <sehicmuhammed7@gmail.com>
2025-09-19 21:07:19 +0200

    v1.4.0

--- src/components/Preloader.jsx
+++ src/components/Preloader.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState, useRef } from "react";
 import baseGaugeOptions from "./gauges/baseGauge";
 
-const appVersion = 'v1.3.8';
+const appVersion = 'v1.4.0';
 
 export default function Preloader() {
     const [visible, setVisible] = useState(true);
